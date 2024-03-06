d5b9bfe5d0a68791a0869510c4c3b6dc46620157
n1999ck <millere9999@gmail.com>
2024-03-05 19:09:31 -0500

    Combining ???

--- pokemon/src/pages/Start.jsx
+++ pokemon/src/pages/Start.jsx
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/esm/Button";
 import Col from "react-bootstrap/esm/Col";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
+import { initializeGame } from "../backend/GameEngine";
 function Start () {
     return (
         <Container><Row>
